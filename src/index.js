fd19ed80b97a3df95f990fa7c48681e1527a8373
AndreykaMen <aodinmg@gmail.com>
2022-02-26 13:14:50 +0600

    Добавил новый компонент TopBar , разметил список ссылок меню.Добавил линк на css

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
 import Routes from "routes";
+import TopBar from "components/topbar"
 
 const App = () => {
   return (
       <div>
         <h3>Welcome to hooks</h3>
         <BrowserRouter>
+          <TopBar/>
           <Routes />
         </BrowserRouter>
       </div>
