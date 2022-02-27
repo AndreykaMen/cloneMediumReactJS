3924859ecbb68b9025ec9e2b925f572d1f4adf01
AndreykaMen <aodinmg@gmail.com>
2022-02-27 18:10:35 +0600

    Добавил новый хук из новой версии Routers -> useLocation, useParams, получаю местоположение.Создал проверку на каком адресе нахожусь isLogin

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Routes from "routes";
-import TopBar from "components/topbar"
+import TopBar from "components/topbar";
 
 const App = () => {
   return (
-      <div>
-        <BrowserRouter>
-          <TopBar/>
-          <Routes />
-        </BrowserRouter>
-      </div>
+      <BrowserRouter>
+        <TopBar/>
+        <Routes/>
+      </BrowserRouter>
   );
 };
 
